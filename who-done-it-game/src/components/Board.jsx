45c4789fd62056433df2456905c5c80630b95ece
Allan Kane <allanhfc@yahoo.co.uk>
2022-02-08 15:38:44 +0000

    prototype state set up, killer, weapon and location selection functionality added

---
+++ who-done-it-game/src/components/Board.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Board = () => {
+  return (
+    <main class="board">
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+      <div class="square"></div>
+    </main>
+  );
+};
+
+export default Board;
--- who-done-it-game/src/components/GameMain.jsx
+++ who-done-it-game/src/components/GameMain.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Board from "./Board";
 
 const GameMain = () => {
   const [suspects, setSuspects] = useState([
@@ -30,21 +31,32 @@ const GameMain = () => {
     "Conservatory",
   ]);
 
+  const murderer = suspects[Math.floor(Math.random() * suspects.length)];
+
+  const murderWeapon = weapons[Math.floor(Math.random() * weapons.length)];
+
+  const murderLocation =
+    locations[Math.floor(Math.random() * locations.length)];
+
   return (
     <div>
       <h3>SUSPECTS</h3>
       <h1>MAIN GAME SCREEN</h1>
+      <Board />
       {suspects.map((suspect) => {
         return <li>{suspect}</li>;
       })}
+      <h3>MURDERER: {murderer}</h3>
       <h3>WEAPONS</h3>
       {weapons.map((weapon) => {
         return <li>{weapon}</li>;
       })}
+      <h3>MURDER WEAPON: {murderWeapon}</h3>
       <h3>LOCATIONS</h3>
       {locations.map((locations) => {
         return <li>{locations}</li>;
       })}
+      <h3>MURDER LOCATION: {murderLocation}</h3>
     </div>
   );
 };
