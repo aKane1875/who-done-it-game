3a98060df6369d190c7a52f09a67a6d24e6cd969
Allan Kane <allanhfc@yahoo.co.uk>
2022-02-09 11:40:20 +0000

    can generate random witness on board on keydown

--- who-done-it-game/src/components/Board.jsx
+++ who-done-it-game/src/components/Board.jsx
@@ -1,28 +1,126 @@
 import React from "react";
 
 const Board = () => {
+  const board = document.getElementsByClassName("board");
+  const squares = [...document.getElementsByClassName("square")];
+  // const squares = board[0].childNodes;
+  // console.log(Array.isArray(squares));
+  console.log(squares);
+
+  const randomWitness = () => {
+    squares.map((square) => {
+      if (square.classList.contains("witness")) {
+        square.classList.remove("witness");
+      }
+    });
+    squares[Math.floor(Math.random() * squares.length)].classList.add(
+      "witness"
+    );
+  };
+
+  document.addEventListener("keydown", randomWitness);
   return (
-    <main class="board">
-      <div class="square"></div>
-      <div class="square"></div>
-      <div class="square"></div>
-      <div class="square"></div>
-      <div class="square"></div>
-      <div class="square"></div>
-      <div class="square"></div>
-      <div class="square"></div>
-      <div class="square"></div>
-      <div class="square"></div>
-      <div class="square"></div>
-      <div class="square"></div>
-      <div class="square"></div>
-      <div class="square"></div>
-      <div class="square"></div>
-      <div class="square"></div>
-      <div class="square"></div>
-      <div class="square"></div>
-      <div class="square"></div>
-      <div class="square"></div>
+    <main className="board">
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
     </main>
   );
 };
--- who-done-it-game/src/components/GameMain.jsx
+++ who-done-it-game/src/components/GameMain.jsx
@@ -44,17 +44,17 @@ const GameMain = () => {
       <h1>MAIN GAME SCREEN</h1>
       <Board />
       {suspects.map((suspect) => {
-        return <li>{suspect}</li>;
+        return <li key={suspect}>{suspect}</li>;
       })}
       <h3>MURDERER: {murderer}</h3>
       <h3>WEAPONS</h3>
       {weapons.map((weapon) => {
-        return <li>{weapon}</li>;
+        return <li key={weapon}>{weapon}</li>;
       })}
       <h3>MURDER WEAPON: {murderWeapon}</h3>
       <h3>LOCATIONS</h3>
       {locations.map((locations) => {
-        return <li>{locations}</li>;
+        return <li key={locations}>{locations}</li>;
       })}
       <h3>MURDER LOCATION: {murderLocation}</h3>
     </div>
